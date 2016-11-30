93f78753ba8b8e470126b293a2a73884ef8f255f
jason00111 <emberleyjason@gmail.com>
2016-11-30 15:19:35 -0800

    All chapter 5 tests passing

--- exercises/compose/compose_exercises.js
+++ exercises/compose/compose_exercises.js
@@ -1,7 +1,7 @@
 require('../../support');
 var _ = require('ramda');
 var accounting = require('accounting');
-  
+
 // Example Data
 var CARS = [
     {name: "Ferrari FF", horsepower: 660, dollar_value: 700000, in_stock: true},
@@ -15,15 +15,27 @@ var CARS = [
 // Exercise 1:
 // ============
 // use _.compose() to rewrite the function below. Hint: _.prop() is curried.
-var isLastInStock = function(cars) {
-  var reversed_cars = _.last(cars);
-  return _.prop('in_stock', reversed_cars)
-};
+
+// var reversed_cars = _.last;
+//
+// var prop = _.prop('in_stock');
+//
+// var isLastInStock = compose(prop, reversed_cars);
+
+
+var isLastInStock = _.compose(_.prop('in_stock'), _.last);
 
 // Exercise 2:
 // ============
 // use _.compose(), _.prop() and _.head() to retrieve the name of the first car
-var nameOfFirstCar = undefined;
+
+// var first = _.head
+//
+// var prop = _.prop('name')
+//
+// var nameOfFirstCar = _.compose(prop, first);
+
+var nameOfFirstCar = _.compose(_.prop('name'), _.head);
 
 
 // Exercise 3:
@@ -31,10 +43,11 @@ var nameOfFirstCar = undefined;
 // Use the helper function _average to refactor averageDollarValue as a composition
 var _average = function(xs) { return reduce(add, 0, xs) / xs.length; }; // <- leave be
 
-var averageDollarValue = function(cars) {
-  var dollar_values = map(function(c) { return c.dollar_value; }, cars);
-  return _average(dollar_values);
-};
+// var dollarValue = _.map(_.prop('dollar_value'))
+//
+// var averageDollarValue = compose(_average, dollarValue)
+
+var averageDollarValue = _.compose(_average, _.map(_.prop('dollar_value')));
 
 
 // Exercise 4:
@@ -43,30 +56,58 @@ var averageDollarValue = function(cars) {
 
 var _underscore = replace(/\W+/g, '_'); //<-- leave this alone and use to sanitize
 
-var sanitizeNames = undefined;
+// var toLower = _.map(function(str) {
+//   return str.toLowerCase()
+// })
+//
+// var sanitizeNames = _.compose(_.map(_underscore), toLower, _.map(_.prop('name')));
+
+
+var sanitizeNames = _.map(_.compose(_underscore, toLowerCase, _.prop('name')));
 
 
 // Bonus 1:
 // ============
 // Refactor availablePrices with compose.
 
-var availablePrices = function(cars) {
-  var available_cars = _.filter(_.prop('in_stock'), cars);
-  return available_cars.map(function(x){
-    return accounting.formatMoney(x.dollar_value)
-  }).join(', ');
-};
+// var availablePrices = function(cars) {
+//   var available_cars = _.filter(_.prop('in_stock'), cars);
+//   return available_cars.map(function(x){
+//     return accounting.formatMoney(x.dollar_value)
+//   }).join(', ');
+// };
+
+var available_cars = _.filter(_.prop('in_stock'));
+
+var formatMoney = _.map(compose(accounting.formatMoney, _.prop('dollar_value')))
+
+var availablePrices = _.compose(join(', '), formatMoney, available_cars)
 
 
 // Bonus 2:
 // ============
 // Refactor to pointfree. Hint: you can use _.flip()
 
-var fastestCar = function(cars) {
-  var sorted = _.sortBy(function(car){ return car.horsepower }, cars);
-  var fastest = _.last(sorted);
-  return fastest.name + ' is the fastest';
-};
+// var fastestCar = function(cars) {
+//   var sorted = _.sortBy(function(car){ return car.horsepower }, cars);
+//   var fastest = _.last(sorted);
+//   return fastest.name + ' is the fastest';
+// };
+
+// var sort = _.sortBy(_.prop('horsepower'))
+//
+// var fastestName = _.compose(_.prop('name'), _.last)
+//
+// var konkat = function(name) {
+//   return name + ' is the fastest'
+// }
+//
+// var fastestCar = compose(konkat, fastestName, sort)
+
+var fastestCar = compose(_.flip(_.concat)(' is the fastest'),
+                         _.prop('name'),
+                         _.last,
+                         _.sortBy(_.prop('horsepower')))
 
 
 module.exports = { CARS: CARS,
--- support.js
+++ support.js
@@ -39,7 +39,7 @@ function curry(fx) {
 }
 
 compose = function() {
-  var fns = toArray(arguments),
+  var fns = arguments,
       arglen = fns.length;
 
   return function(){
