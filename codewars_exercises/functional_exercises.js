48664d3363c58cc66a9c3ffb03da161c0c4d21d7
jason00111 <emberleyjason@gmail.com>
2016-12-02 16:39:54 -0800

    one more codewars exercise

--- codewars_exercises/functional_exercises.js
+++ codewars_exercises/functional_exercises.js
@@ -37,7 +37,7 @@ function flattenAndSort(arrays) {
       result.push(element)
     })
   })
-  
+
   return result.sort(function(a,b) {
     return a - b
   });
@@ -53,8 +53,20 @@ var parseNumbers = function(intStrs) {
   var results = [];
   for (var i = 0; i < intStrs.length; i++) {
     results.push(parseInt(intStrs[i]));
-  } 
+  }
   return results;
 }
 
-  
+// Challenge 4
+
+// Make the sum() function return the sum of the values in the passed in array. Your solution must use the reduce() function of the built-in javascript Array object. If you're not familiar with reduce(), reading over the documentation may help.
+
+// Use reduce() to calculate the sum of the values in an array
+
+function sum(array) {
+  // Use array.reduce() to calculate and return the
+  // sum of the values in array.
+  return array.reduce(function(prev, cur) {
+    return prev + cur
+  })
+}
