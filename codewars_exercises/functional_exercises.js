9e8619232a6f69e57ff93b92931a3e78bb4200f8
jason00111 <emberleyjason@gmail.com>
2016-12-02 17:12:02 -0800

    Add challenge 6

--- codewars_exercises/functional_exercises.js
+++ codewars_exercises/functional_exercises.js
@@ -70,3 +70,95 @@ function sum(array) {
     return prev + cur
   })
 }
+
+// Challenge 5
+
+// This time we want to write calculations using functions and get the results. Let's have a look at some examples:
+//
+// seven(times(five())); // must return 35
+// four(plus(nine())); // must return 13
+// eight(minus(three())); // must return 5
+// six(dividedBy(two())); // must return 3
+// Requirements:
+//
+// There must be a function for each number from 0 ("zero") to 9 ("nine")
+// There must be a function for each of the following mathematical operations: plus, minus, times, dividedBy (divided_by in Ruby)
+// Each calculation consist of exactly one operation and two numbers
+// The most outer function represents the left operand, the most inner function represents the right operand
+
+
+// Calculating with Functions
+
+function zero(func) {
+  return func ? func(0) : 0
+}
+function one(func) {
+  return func ? func(1) : 1
+}
+function two(func) {
+  return func ? func(2) : 2
+}
+function three(func) {
+  return func ? func(3) : 3
+}
+function four(func) {
+  return func ? func(4) : 4
+}
+function five(func) {
+  return func ? func(5) : 5
+}
+function six(func) {
+  return func ? func(6) : 6
+}
+function seven(func) {
+  return func ? func(7) : 7
+}
+function eight(func) {
+  return func ? func(8) : 8
+}
+function nine(func) {
+  return func ? func(9) : 9
+}
+
+function plus(rightNumber) {
+  return function (leftNumber) {
+    return leftNumber + rightNumber
+  }
+}
+function minus(rightNumber) {
+  return function (leftNumber) {
+    return leftNumber - rightNumber
+  }
+}
+function times(rightNumber) {
+  return function (leftNumber) {
+    return leftNumber * rightNumber
+  }
+}
+function dividedBy(rightNumber) {
+  return function (leftNumber) {
+    return leftNumber / rightNumber
+  }
+}
+
+// Challenge 6
+
+// Function composition is a mathematical operation that mainly presents itself in lambda calculus and computability. It is explained well here, but this is my explanation, in simple mathematical notation:
+//
+// f3 = compose( f1 f2 )
+//    Is equivalent to...
+// f3(a) = f1( f2( a ) )
+// Your task is to create a compose function to carry out this task, which will be passed two functions or lambdas. Ruby functions will be passed, and should return, either a proc or a lambda. Remember that the resulting composed function may be passed multiple arguments!
+//
+// compose(f , g)(x)
+// => f( g( x ) )
+// This kata is not available in haskell; that would be too easy!
+
+// Function Composition
+
+function compose(f,g) {
+  // Compose the two functions here!
+  return function(...args) {
+    return f(g.apply(g, args))
+  }
+}
